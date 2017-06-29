9a7940f8d041f3b785fee9bafe1924b6075d45b6
Maksym <m.fomichov@gmail.com>
2017-06-29 17:36:17 +0300

    первая программа закончена

--- src/com/gmail/fomichov/m/homework/Main.java
+++ src/com/gmail/fomichov/m/homework/Main.java
@@ -1,7 +1,20 @@
 package com.gmail.fomichov.m.homework;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by mfomi on 29.06.2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int data[] = new int[4];
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("Введите " + (i + 1) + " число:");
+            data[i] = scanner.nextInt();
+        }
+        Arrays.sort(data);
+        System.out.println("Максимально введенное число: " + data[3]);
+    }
 }
